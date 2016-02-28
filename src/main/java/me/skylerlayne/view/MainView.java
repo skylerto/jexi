518cf5399155e33dcb4c75fea58810bd7d1dd5d4
Skyler Layne <skylerclayne@gmail.com>
2016-02-28 18:09:33 -0500

    'new' menubar item listener

--- src/main/java/me/skylerlayne/view/MainView.java
+++ src/main/java/me/skylerlayne/view/MainView.java
@@ -1,10 +1,15 @@
 package me.skylerlayne.view;
 
 import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
 import javax.swing.JMenuBar;
 import javax.swing.JToolBar;
+
+import me.skylerlayne.view.listeners.NewActionListener;
+
 import java.awt.BorderLayout;
 import javax.swing.JPanel;
 import javax.swing.JEditorPane;
@@ -46,47 +51,48 @@ public class MainView {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+
 		JEditorPane editorPane = new JEditorPane();
 		frame.getContentPane().add(editorPane, BorderLayout.CENTER);
-		
+
 		JMenuBar menuBar = new JMenuBar();
 		menuBar.setBackground(Color.LIGHT_GRAY);
 		frame.setJMenuBar(menuBar);
-		
+
 		JMenu mnFile = new JMenu("File");
 		mnFile.setBackground(Color.LIGHT_GRAY);
 		menuBar.add(mnFile);
-		
+
 		JMenuItem mntmNew = new JMenuItem("New");
 		mnFile.add(mntmNew);
-		
+		mntmNew.addActionListener(new NewActionListener());
+
 		JMenuItem mntmOpen = new JMenuItem("Open");
 		mnFile.add(mntmOpen);
-		
+
 		JMenuItem mntmSave = new JMenuItem("Save");
 		mnFile.add(mntmSave);
-		
+
 		JMenuItem mntmSaveAs = new JMenuItem("Save As");
 		mnFile.add(mntmSaveAs);
-		
+
 		JMenuItem mntmExportLatex = new JMenuItem("Export");
 		mnFile.add(mntmExportLatex);
-		
+
 		JMenu mnEdit = new JMenu("Edit");
 		mnEdit.setBackground(Color.LIGHT_GRAY);
 		menuBar.add(mnEdit);
-		
+
 		JMenuItem mntmNewMenuItem = new JMenuItem("Plain Text");
 		mnEdit.add(mntmNewMenuItem);
-		
+
 		JMenuItem mntmMarkdown = new JMenuItem("Markdown");
 		mnEdit.add(mntmMarkdown);
-		
+
 		JMenu mnHelp = new JMenu("Help");
 		mnHelp.setBackground(Color.LIGHT_GRAY);
 		menuBar.add(mnHelp);
-		
+
 		JMenuItem mntmShowHelp = new JMenuItem("Show Help");
 		mnHelp.add(mntmShowHelp);
 	}
---
+++ src/main/java/me/skylerlayne/view/listeners/NewActionListener.java
@@ -0,0 +1,21 @@
+package me.skylerlayne.view.listeners;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import me.skylerlayne.view.MainView;
+
+public class NewActionListener implements ActionListener {
+
+	/**
+	 * Perform the New operation.
+	 * 
+	 * Create a new instance of the app?
+	 */
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		MainView mv = new MainView();
+		mv.main(null);
+	}
+
+}
